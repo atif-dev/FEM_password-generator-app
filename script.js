1fbaa4f54b7359bd879882fd4da1a087a0d29ff8
atif-dev <ati1254@gmail.com>
2026-03-10 07:23:00 +0500

    exception message modified

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ function showPasswordStrength(){
 function copyToClipboard() {
   const samplePassword = "P4$5W0rD!";
   if(password.innerText === samplePassword){
-    error.innerHTML = "--Please Generate Another Password--"
+    error.innerHTML = "--Please Generate New Password--"
     return;
   }
   navigator.clipboard.writeText(password.innerText);
